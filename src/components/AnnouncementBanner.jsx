8c03417a0d836fec83a2ec86a605176ff339e432
Aye Zin Htun <ayezinhtun9@gmail.com>
2025-10-27 16:48:39 +0630

    update in

--- src/components/AnnouncementBanner.jsx
+++ src/components/AnnouncementBanner.jsx
@@ -1,35 +1,13 @@
 import React, { useState, useEffect } from "react";
-import banner from '../assets/banner.png';
+import banner from '../assets/image.png';
 
 export default function AnnouncementBanner({ onBannerHeight }) {
   const [hidden, setHidden] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
 
-  // const [endTime] = useState(() => {
-  //   const saved = localStorage.getItem("bannerEndTime");
-  //   if (saved && !isNaN(Number(saved))) return Number(saved);
-  //   const now = new Date().getTime();
-  //   const newEnd = now + 10 * 24 * 60 * 60 * 1000; // 10 days in ms
-  //   localStorage.setItem("bannerEndTime", newEnd.toString());
-  //   return newEnd;
-  // });
-
-    //   const [endTime] = useState(() => {
-    //   const savedStart = localStorage.getItem("bannerStartTime");
-    //   let start;
-    //   if (savedStart && !isNaN(Number(savedStart))) {
-    //     start = Number(savedStart);
-    //   } else {
-    //     start = new Date().getTime();
-    //     localStorage.setItem("bannerStartTime", start.toString());
-    //   }
-    //   return start + 1 * 24 * 60 * 60 * 1000; 
-    //   }
-
-    // );
 
   const [endTime] = useState(() => {
-      return new Date("2025-10-28T10:00:00").getTime(); 
+      return new Date("2025-11-05T10:00:00").getTime(); 
   });
 
 
@@ -79,18 +57,18 @@ export default function AnnouncementBanner({ onBannerHeight }) {
   return (
     <div
       id="announcement-banner"
-      className={`w-full transform transition-transform duration-300 py-2 ease-in-out ${
+      className={`w-full transform transition-transform duration-300 ease-in-out ${
         hidden ? "-translate-y-full" : "translate-y-0"
       }`}
-      style={{ backgroundColor: "#1F2937", color: "#fff" }}
+      style={{ backgroundColor: "linear-gradient(135deg, #E0F2FE 0%, #B9D9F8 100%)", color: "#fff" }}
     >
-      <div className="max-w-7xl mx-auto px-4 py-2 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto px-4 grid grid-cols-3 items-center">
         {/* Left: Logo */}
-        <div className="flex-shrink-0">
-          <img src={banner} alt="Banner Logo" className="h-24 w-auto rounded" />
+        <div className="flex justify-center">
+          <img src={banner} alt="Banner Logo" className="h-40 w-auto pt-[2px]" />
         </div>
 
-        <div className="flex flex-col items-center text-center">
+        <div className="flex flex-col items-center text-center pt-[2px]">
           <div className="flex flex-col items-center text-center">
             <div className="flex items-center gap-3 mb-1">
               {/* Days */}
@@ -115,8 +93,8 @@ export default function AnnouncementBanner({ onBannerHeight }) {
               </div>
             </div>
 
-            <div className="text-sm md:text-base font-medium text-white pt-3">
-              One Cloud Next-Gen Launching Soon — Get Ready to Deploy Powerful Cloud Instances!
+            <div className="text-sm md:text-base font-semibold text-primary pt-3">
+              As part of our soft launch, we’re offering free cloud credits up to 9 lakhs  to early customers who register with us. Slots are limited, secure your access before registration closes on 5 November 2025.
             </div>
           </div>
 
@@ -124,7 +102,7 @@ export default function AnnouncementBanner({ onBannerHeight }) {
         </div>
 
         {/* Right: Button with three-line icons */}
-        <div className="flex items-center gap-2">
+        <div className="flex items-center justify-end gap-2">
           {/* Left three-line icon (mirrored) */}
           <div className="flex flex-col justify-center gap-[3px]">
             <div className="w-4 h-[2px] bg-white rotate-[20deg] origin-right"></div>
@@ -134,10 +112,12 @@ export default function AnnouncementBanner({ onBannerHeight }) {
 
           {/* Button */}
           <a
-            href="#"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSeQ89PUvgi1hF4-6XO-KzBwdD_h4WFoj54p3HOIc_mh9DfzBA/viewform?usp=sharing&ouid=106599595537133284739"
+            target="_blank"
+            rel="noopener noreferrer"
             className="bg-[#ffaa04] text-sm md:text-base relative z-10 text-white font-semibold py-3 px-5 md:px-6 rounded-lg hover:bg-[#e69500] transition duration-300 text-center"
           >
-            Start Here
+            Register Here
           </a>
 
           {/* Right three-line icon */}
