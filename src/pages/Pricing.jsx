87ed8b9863c479ade914f57e4cab4d724b6b263d
Aye Zin Htun <ayezinhtun9@gmail.com>
2026-02-09 10:27:55 +0630

    update in scrollspy

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -8,19 +8,7 @@ import MemoryDense from "./MemoryDense";
 import MemoryDensePlus from "./MemoryDensePlus";
 import { Cpu, Database, Grid, PieChart, Scale, Server, Zap } from "lucide-react";
 
-const Pricing = ({ bannerHeight }) => {
-  const { hash } = useLocation();
-  useEffect(() => {
-    if (hash !== "#pricing" && hash !== "#prices") return;
-
-    const element = document.querySelector(hash);
-    if (element) {
-      const yOffset = hash === "#prices" ? 0 : bannerHeight;
-      const y = element.getBoundingClientRect().top + window.pageYOffset - yOffset;
-      window.scrollTo({ top: y, behavior: "smooth" });
-    }
-  }, [hash, bannerHeight]);
-
+const Pricing = () => {
   const pricings = [
     { id: "section1", title: "Extra CPU Optimized", description: "Start at 24 MMK/hour", icon: <Zap className="w-7 h-7" />, },
     { id: "section2", title: "CPU Optimized", description: "Start at 40 MMK/hour", icon: <Cpu className="w-7 h-7" />, },
@@ -122,9 +110,9 @@ const Pricing = ({ bannerHeight }) => {
                     <a href={`#${p.id}`} className="flex flex-col">
                       <li
                         key={p.id}
-                        className={`flex rounded-md items-center gap-2 px-2 transition-all duration-200 ${isActive
-                          ? "border-none bg-white/90 shadow-md shadow-blue-400"
-                          : "border border-gray-300 bg-white/80"
+                        className={`flex items-center gap-2 px-2 transition-all duration-200 ${isActive
+                          ? "border-none"
+                          : "border"
                           }`}
                       >
                         <div className={`p-2 rounded-lg ${isActive ? "bg-secondary text-white" : "border border-secondary text-secondary"}`}>
