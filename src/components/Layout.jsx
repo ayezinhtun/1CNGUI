578da9dbdbdfdd351b75df56f8b98406a10e4443
Aye Zin Htun <ayezinhtun9@gmail.com>
2026-02-19 10:07:37 +0630

    update in main

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,21 +2,15 @@ import React, { useRef, useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
-import AnnouncementBanner from "./AnnouncementBanner";
 
 export function Layout({ children }) {
   
-    const [bannerHeight, setBannerHeight] = useState(0);
   return (
     <div>
-
       
-      
-      {/* <AnnouncementBanner onBannerHeight={setBannerHeight} /> */}
-      {/* <AnnouncementBanner/> */}
-      {/* <Navbar/> */}
-        <Navbar bannerHeight={bannerHeight} />
-      <main>{React.cloneElement(children, { bannerHeight })}</main>
+         
+      <Navbar/>
+      <main>{children}</main>
       <Footer />
       <Outlet />
     </div>
