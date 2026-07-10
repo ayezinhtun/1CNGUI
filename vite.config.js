103035898d4e30d5c519e7f1885da170c27a9890
Aye Zin Htun <ayezinhtun9@gmail.com>
2026-07-10 08:47:43 +0630

    update text

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    fs: {
-      strict: false
-    },
-    configureServer(server) {
-      server.middlewares.use((req, res, next) => {
-        if (req.url?.endsWith('.txt')) {
-          res.setHeader('Content-Disposition', 'attachment')
-        }
-        next()
-      })
+    headers: {
+      'Content-Disposition': 'attachment'
     }
   }
 })
